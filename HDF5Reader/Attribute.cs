b7d9006d609493ffcaf18df69efc19670fb6e53c
Niels Abildgaard <niels.abildgaard@gmail.com>
2014-05-24 21:36:48 +0200

    First Commit

---
+++ HDF5Reader/Attribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDF5Wrapper
+{
+    /// <summary>
+    /// An attribute describes the type of a field.
+    /// </summary>
+    abstract class Attribute
+    {
+        public readonly Encoding Encoding = new ASCIIEncoding();
+
+        public readonly string Name;
+        public readonly int Length;
+
+        public Attribute(string name, int length)
+        {
+            Name = name;
+            Length = length;
+        }
+
+        public abstract object Parse(byte[] data);
+    }
+}
---
+++ HDF5Reader/Container.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDF5DotNet;
+
+namespace HDF5Wrapper
+{
+    /// <summary>
+    /// Represents a file or group.
+    /// </summary>
+    abstract class Container
+    {
+        public abstract H5FileOrGroupId Id {
+            get;
+        }
+
+        public Group GetGroup(string groupname)
+        {
+            return new Group(this, groupname);
+        }
+
+        public CompoundDataset GetCompoundDataset(string datasetname)
+        {
+            return new CompoundDataset(this, datasetname);
+        }
+
+        public ScalarDataset GetScalarDataset(string datasetname)
+        {
+            return new ScalarDataset(this, datasetname);
+        }
+    }
+}
